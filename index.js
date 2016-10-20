498e41f5b2144f8b882750326cfdb79567a7eeae
Mugsy Carter <mugsycarter@gmail.com>
2016-10-20 16:55:08 -0700

    Eslint fix fixed.  New correct npm test, pretest, and lint scripts added.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-const periodicTable = require('../lib/periodicTable');
+const periodicTable = require('./lib/periodicTable');
 const rl = require('readline');
 
-  var i = rl.createInterface(process.stdin, process.stdout, null);
-  prefix = 'Periodic Table> ';
-  i.question(prefix + 'Enter a capitalized element name to see its information.', function(answer) {
+console.log('in index file');
+var i = rl.createInterface(process.stdin, process.stdout, null);
+prefix = 'Periodic Table> ';
+i.question(prefix + 'Enter a capitalized element name to see its information.', function(answer) {
   periodicTable.findElementByName(answer);
   i.close();
   process.stdin.destroy();
-  });
+});
